259b144085671bd6303b0f3a42f7c02e6493849a
Mojtaba Yekta <mojtaba.a63@gmail.com>
2023-05-22 00:43:44 +0200

    working on animations

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.less'],
 })
 export class HomepageComponent {
-  imageUrls: string[] = ['Apple', 'Orange', 'Banana'];
+  imageUrls: string[];
 
   constructor() {
     this.imageUrls = [
